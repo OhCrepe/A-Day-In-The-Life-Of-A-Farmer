cee72d374f80ca839a89979db7e4781cb5dd2e64
Thorn <tt16786@essex.ac.uk>
2019-11-23 19:51:01 +0000

    Initialized project and made the a moveable character

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public GameObject target;
+    public float horBoundary, verBoundary;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = this.transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.transform.position + offset;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -horBoundary, horBoundary),
+                                        Mathf.Clamp(transform.position.y, -verBoundary, verBoundary),
+                                        transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerWalking.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalking : MonoBehaviour
+{
+
+    public float speed,
+                 horBoundary,
+                 verBoundary;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        SetVelocity();
+        ClampPosition();
+
+    }
+
+    private void SetVelocity(){
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(x, y);
+        movement.Normalize();
+        movement *= speed;
+        rb.velocity = movement;
+
+    }
+
+    private void ClampPosition(){
+
+        rb.position = new Vector2(Mathf.Clamp(rb.position.x, -horBoundary, horBoundary),
+                                    Mathf.Clamp(rb.position.y, - verBoundary, verBoundary));
+
+    }
+
+}
