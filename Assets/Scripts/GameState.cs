b51752997b8f34f27611f8dd93816a5e6caab2de
Thorn <tt16786@essex.ac.uk>
2019-11-23 21:34:36 +0000

    Cows can be milked

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameState : MonoBehaviour
 {
 
     public float time;
+    public Text milkText;
 
     private SpriteRenderer cycle;
+    private int milkCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -21,4 +24,14 @@ public class GameState : MonoBehaviour
     {
         cycle.color = new Color(1f, 1f, 1f, time);
     }
+
+    public void CollectMilk(){
+        milkCounter++;
+        UpdateMilkText();
+    }
+
+    private void UpdateMilkText(){
+        milkText.text = "x " + milkCounter;
+    }
+
 }
---
+++ Assets/Scripts/MilkCow.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkCow : MonoBehaviour
+{
+
+    public float minDelay, maxDelay, collectDistance;
+
+    private GameObject milkSymbol, player;
+    private bool collectable;
+    private GameState gamestate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        milkSymbol = transform.Find("Milk").gameObject;
+        collectable = true;
+        gamestate = GameObject.Find("GameState").GetComponent<GameState>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(collectable){
+            milkSymbol.SetActive(true);
+        }else{
+            milkSymbol.SetActive(false);
+        }
+
+    }
+
+    IEnumerator collectMilkAndDelay(){
+
+        gamestate.CollectMilk();
+        collectable = false;
+        float delay = Random.Range(minDelay, maxDelay);
+        yield return new WaitForSeconds(delay);
+        collectable = true;
+
+    }
+
+    void OnMouseDown(){
+        if(collectable){
+            if(Vector3.Distance(player.transform.position, transform.position) < collectDistance){
+                StartCoroutine(collectMilkAndDelay());
+            }
+        }
+    }
+
+}
