93f467eaabead94667abf80645759cdfad3167fa
Thorn <tt16786@essex.ac.uk>
2019-11-23 20:53:05 +0000

    Added daynight cycle, grass background, milk counter

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+
+    public float time;
+
+    private SpriteRenderer cycle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0f;
+        cycle = GameObject.Find("DayNight").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cycle.color = new Color(1f, 1f, 1f, time);
+    }
+}
--- Assets/Scripts/PlayerWalking.cs
+++ Assets/Scripts/PlayerWalking.cs
@@ -22,7 +22,7 @@ public class PlayerWalking : MonoBehaviour
     {
 
         SetVelocity();
-        ClampPosition();
+        //ClampPosition();
 
     }
 
