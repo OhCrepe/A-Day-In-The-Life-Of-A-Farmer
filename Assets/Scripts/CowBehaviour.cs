18b7cd21edd2abc3afc410c7a1aad69df7be45c5
Thorn <tt16786@essex.ac.uk>
2019-11-24 15:01:21 +0000

    Cows now breed and make small cows, and it's pretty cute

--- Assets/Scripts/CowBehaviour.cs
+++ Assets/Scripts/CowBehaviour.cs
@@ -6,21 +6,37 @@ public class CowBehaviour : MonoBehaviour
 {
 
     public float speed, minWalkDelay, maxWalkDelay;
+    public bool breeding;
 
     private Animator anim;
     private Vector3 destination, previousLocation;
     private bool moving;
     private GameState gamestate;
     private Rigidbody2D rb;
+    public GameObject mate;
+    public GameObject child;
 
-    void Start(){
+    private float growth;
+    public bool adult;
+
+    void Awake(){
         gamestate = GameObject.Find("GameState").GetComponent<GameState>();
         anim = GetComponent<Animator>();
         destination = transform.position;
         previousLocation = transform.position;
         moving = false;
+        breeding = false;
         StartCoroutine(DecideDestination());
+        StartCoroutine(Breeding());
         rb = GetComponent<Rigidbody2D>();
+        growth = 1f;
+        adult = true;
+    }
+
+    public void BeBorn(){
+        growth = 0.5f;
+        adult = false;
+        transform.localScale = new Vector3(growth, growth, 1f);
     }
 
     public void WakeUp(){
@@ -36,8 +52,21 @@ public class CowBehaviour : MonoBehaviour
     void Update()
     {
 
+        if(!adult){
+            growth += Time.deltaTime/200;
+            if(growth >= 1f){
+                growth = 1f;
+                adult = true;
+            }
+            transform.localScale = new Vector3(growth, growth, 1f);
+        }
         rb.velocity = new Vector3();
-        if(moving && gamestate.day){
+        if(breeding && gamestate.day){
+            transform.position = Vector3.MoveTowards(transform.position, mate.transform.position, speed*Time.deltaTime);
+            if(Vector3.Distance(transform.position, mate.transform.position) <= 1.5f){
+                Breed();
+            }
+        }else if(moving && gamestate.day){
             transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
             if(Vector3.Distance(transform.position, destination) < 0.01f || Vector3.Distance(transform.position, previousLocation) < 0.01f){
                 anim.SetTrigger("Stand");
@@ -71,6 +100,51 @@ public class CowBehaviour : MonoBehaviour
             moving = true;
             anim.SetTrigger("Walk");
         }
+    }
+
+    IEnumerator Breeding(){
+
+        yield return new WaitForSeconds(Random.Range(40f, 60f));
+        if(!breeding && adult){
+            GameObject[] cows = GameObject.FindGameObjectsWithTag("Cow");
+            foreach(GameObject cow in cows){
+                if(cow != this.gameObject){
+                    if(!cow.GetComponent<CowBehaviour>().breeding){
+                        breeding = true;
+                        mate = cow;
+                        cow.GetComponent<CowBehaviour>().BreedWith(gameObject);
+                        anim.SetTrigger("Walk");
+                        break;
+                    }
+                }
+            }
+            StartCoroutine(Breeding());
+        }
+
+    }
+
+    public void BreedWith(GameObject cow){
+        breeding = true;
+        mate = cow;
+        anim.SetTrigger("Walk");
+    }
+
+    private void Breed(){
+        Vector3 distance = mate.transform.position - transform.position;
+        distance = distance * 0.5f;
+        Vector3 childPos = transform.position + distance;
+        GameObject bornChild = Instantiate(child, childPos, transform.rotation);
+        bornChild.GetComponent<CowBehaviour>().BeBorn();
+        mate.GetComponent<CowBehaviour>().NotBreeding();
+        NotBreeding();
+
+    }
+
+    public void NotBreeding(){
+        breeding = false;
+        mate = null;
+        anim.SetTrigger("Stand");
+        StartCoroutine(Breeding());
     }
 
 }
--- Assets/Scripts/MilkCow.cs
+++ Assets/Scripts/MilkCow.cs
@@ -24,7 +24,7 @@ public class MilkCow : MonoBehaviour
     void Update()
     {
 
-        if(collectable && gamestate.day && !GetComponent<CowBehaviour>().breeding){
+        if(collectable && gamestate.day && !GetComponent<CowBehaviour>().breeding && GetComponent<CowBehaviour>().adult){
             milkSymbol.SetActive(true);
         }else{
             milkSymbol.SetActive(false);
@@ -43,7 +43,7 @@ public class MilkCow : MonoBehaviour
     }
 
     void OnMouseDown(){
-        if(collectable && gamestate.day && !GetComponent<CowBehaviour>().breeding){
+        if(collectable && gamestate.day && !GetComponent<CowBehaviour>().breeding && GetComponent<CowBehaviour>().adult){
             if(Vector3.Distance(player.transform.position, transform.position) < collectDistance){
                 StartCoroutine(collectMilkAndDelay());
             }
